215ec323f6271a6991a2b3329729cb4bd928ca98
Anton Carlsson <mail@antoncarlsson.se>
2019-09-22 18:13:43 +0200

    Added Chakra-UI components to the 404 page.

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Heading, Text } from '@chakra-ui/core';
 
 import Layout from '../components/layout';
 import SEO from '../components/seo';
@@ -6,8 +7,20 @@ import SEO from '../components/seo';
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <Box justify="center">
+      <Heading fontFamily="heading" color="gray.700" p={4}>
+        NOT FOUND
+      </Heading>
+      <Box
+        rounded="lg"
+        backgroundColor="gray.50"
+        p={8}
+        border="1px"
+        borderColor="gray.200"
+      >
+        <Text>You just hit a route that doesn&#39;t exist... the sadness.</Text>
+      </Box>
+    </Box>
   </Layout>
 );
 
